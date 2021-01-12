6611f43fcc49bdec85c00f8f4dbb5abc51fba6c1
yutaoyu1998 <yutaoyu.1998@gmail.com>
2021-01-12 09:58:39 +0800

    feat: 委托构造

---
+++ 03创建对象/index4.js
@@ -0,0 +1,47 @@
+/*
+    寄生-构造函数-模式，也就是说寄生在构造函数的一种方法。
+    也就是说打着构造函数的幌子挂羊头卖狗肉，你看创建的实例使用 instanceof 都无法指向构造函数！
+*/
+
+function Person(name) {
+
+    var o = new Object();
+    o.name = name;
+    o.getName = function () {
+        console.log(this.name);
+    };
+
+    return o;
+
+}
+
+var person1 = new Person('kevin');
+console.log(person1 instanceof Person) // false
+console.log(person1 instanceof Object)  // true
+
+/*
+    添加额外方法
+*/
+
+function SpecialArray() {
+    var values = new Array();
+
+    for (var i = 0, len = arguments.length; i < len; i++) {
+        values.push(arguments[i]);
+    }
+
+    values.toPipedString = function () {
+        return this.join("|");
+    };
+    return values;
+}
+
+var colors = new SpecialArray('red', 'blue', 'green');
+var colors2 = SpecialArray('red2', 'blue2', 'green2');
+
+
+console.log(colors);
+console.log(colors.toPipedString()); // red|blue|green
+
+console.log(colors2);
+console.log(colors2.toPipedString()); // red2|blue2|green2
